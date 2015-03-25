874d013482f12f16bf8371cabf9513495380506a
Dustin Collins <dustinrcollins@gmail.com>
2015-03-25 16:08:31 -0400

    [#91126334] Add conf and pem files for demo

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ The Chef flow looks like this:
 1. Base cookbook is used to build an initial image
 2. Conjur identity is side-loaded at node initial
 3. Application cookbook runs on top, can access secrets via Conjur
+
+Note: `files/conjur-demo.pem` is meant to only validate inside instances
+launched via demo-factory.
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,18 @@ include_recipe 'terminal-login::install'
 include_recipe 'terminal-login::sudoers'
 
 include_recipe 'conjur_base::chefdk'
+
+cookbook_file '/etc/conjur.conf' do
+  source 'conjur.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+cookbook_file '/etc/conjur-demo.pem' do
+  source 'conjur-demo.pem'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  sensitive true
+end
