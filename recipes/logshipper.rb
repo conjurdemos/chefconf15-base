86e47f69476536c3a2dca625f07045966c32dc77
Kevin Gilpin <kgilpin@gmail.com>
2015-03-27 15:47:28 -0400

    Add logshipper package, users, and groups

--- recipes/logshipper.rb
+++ recipes/logshipper.rb
@@ -1,3 +1,61 @@
+require 'yaml'
+
+## install package
+
+apt_repository 'conjur' do
+  uri 'http://apt.conjur.s3-website-us-east-1.amazonaws.com'
+  components %w(main)
+  distribution node['lsb']['codename']
+  key "apt.key"
+end
+
+package 'logshipper'
+
+## create the group, user, fifo and logfile
+
+group 'conjur' do
+  action :create
+  append true
+end
+
+user "logshipper" do
+  system true
+  shell '/bin/false'
+  group "conjur"
+end
+
+fifo_path = '/var/run/logshipper'
+if node.etc.group.include? 'syslog'
+  fifo_group = 'syslog'
+else
+  fifo_group = 'root'
+end
+
+bash "mkfifo #{fifo_path}" do
+  not_if { begin
+    s = File.stat(fifo_path)
+    [
+      s.pipe?,
+      (s.mode & 0777 == 0460),
+      s.uid == node.etc.passwd.logshipper.uid,
+      s.gid == node.etc.group[fifo_group].gid,
+    ].all?
+  rescue Errno::ENOENT, NoMethodError
+    false
+  end }
+
+  code """
+    rm -f #{fifo_path}
+    mkfifo --mode=0460 #{fifo_path}
+    chown logshipper:#{fifo_group} #{fifo_path}
+  """
+end
+
+file "/var/log/logshipper.log" do
+  owner 'logshipper'
+  mode '0640'
+end
+
 cookbook_file '/etc/syslog-ng/conf.d/logshipper.conf' do
   source 'logshipper.conf'
   owner 'root'
